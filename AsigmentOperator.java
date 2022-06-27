8d708e1d0f6c2a93729a1b1ac3a30036332716d3
Windows 10 <irobatic28@gmail.com>
2022-06-27 08:51:00 +0300

    asigmentOperators

---
+++ AsigmentOperator.java
@@ -0,0 +1,7 @@
+
+public class AsigmentOperator {
+    public static void main(String[] args) {
+        int variabila = 5;
+        System.out.println("variabila are valoarea " + variabila);
+    }
+}
