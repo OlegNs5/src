8c5440eb8f8ac019a034782399d877edc8e6087e
OlegNs5 <108250777+OlegNs5@users.noreply.github.com>
2022-06-29 06:29:03 +0300

    Delete AsigmentOperator.java

--- AsigmentOperator.java
+++
@@ -1,7 +0,0 @@
-
-public class AsigmentOperator {
-    public static void main(String[] args) {
-        int variabila = 5;
-        System.out.println("variabila are valoarea " + variabila);
-    }
-}
